cdcf2c517f4835ddc5a611f31894e5134cc5bd7a
Ragarr <93985816+Ragarr@users.noreply.github.com>
2022-02-24 18:17:00 +0100

    v

--- esqueleto/phase1.py
+++ esqueleto/phase1.py
@@ -4,22 +4,23 @@ import sys
 class SList2(SList):
 
     def sumLastN(self, n):
-        ...
+
+        pass
     
     #method for inserting a new node in the middle
     def insertMiddle(self, elem):
         
-        ...
+        pass
 
     
     def insertList(self,inputList,start,end):
-        ...
+        pass
 
 
     def reverseK(self,k):
-        ...
+        pass
 
 
     def maximumPair(self):
-        ...
+        pass
  
